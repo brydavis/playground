0d7ea3c0cd29fd72f786d1a29bf4944e0c294e57
brydavis <bryan@neoty.pe>
2015-03-21 01:49:19 -0700

    auto commit

---
+++ compile/example.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{10090, 523, 66134, 1463, 146, 3, 7, 34, 67, 34, 8, 342, 9809, 324}
+	for i := 0; i < 5; i++ {
+		fmt.Println(a)
+	}
+}
---
+++ play.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os/exec"
+)
+
+type Output struct {
+	Code []byte
+	Text []byte
+}
+
+func main() {
+	http.HandleFunc("/", Root)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func Root(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		ioutil.WriteFile("compile/example.go", []byte(r.FormValue("data")), 0700)
+		exec.Command("go", "fmt", "compile/example.go").Run()
+		exec.Command("goimports", "-w", "compile/example.go").Run()
+		err := exec.Command("go", "build", "compile/example.go").Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		out, err := exec.Command("./example").Output()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		view, _ := ioutil.ReadFile("views/base.html")
+		t := template.New("output")
+		t, err = t.Parse(string(view))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		code, _ := ioutil.ReadFile("compile/example.go")
+		err = t.Execute(w, Output{code, out})
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		data, _ := ioutil.ReadFile("form/data.go")
+		view, _ := ioutil.ReadFile("views/base.html")
+
+		// w.Write(view)
+
+		t := template.New("output")
+		t, err := t.Parse(string(view))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// w.Write(out)
+		err = t.Execute(w, Output{[]byte(data), []byte("")})
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+
+}

339463563055037f47dadb1c5cf3919d821470a0
brydavis <bryan@neoty.pe>
2015-03-22 21:57:15 -0700

    auto commit

--- compile/example.go
+++ compile/example.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"strconv"
-)
+import "fmt"
+
+const text = `The quick, brown fox jumps over a lazy dog. DJs flock by when VH1 ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad nymph, for quick jigs vex! Fox nymphs grab quick-jived waltz. Brick quiz whangs jumpy veldt fox. Bright vixens jump; dozy fowl quack. Quick wafting zephyrs vex bold Jim. Quick zephyrs blow, vexing daft Jim. Sex-charged fop blew my junk TV quiz. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Quick, Baz, get my woven flax jodhpurs! "Now fax quiz Jack!" my brave ghost pled. Five quacking zephyrs jolt my wax bed. Flummoxed by job, kvetching W. zaps Iraq. Cozy sphinx waves quart jug of bad milk. A very bad quack might jinx zippy fowls. Few quips galvanized the mock jury box. Quick brown dogs jump over the lazy fox. The jay, pig, fox, zebra, and my wolves quack! Blowzy red vixens fight for a quick jump. Joaquin Phoenix was gazed by MTV for luck. A wizard’s job is to vex chumps quickly in fog. Watch "Jeopardy!", Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just suck!`
 
 func main() {
-
-	for i := 0; i < 50; i++ {
-		var output string
-		if m := math.Mod(float64(i), 3.0); m == 0 {
-			output += "Fizz"
-		}
-
-		if math.Mod(float64(i), 5.0) == 0 {
-			output += "Buzz"
-		}
-
-		if len(output) == 0 {
-			fmt.Print(strconv.Itoa(i))
-
-			// fmt.Print(i)
-		}
-
-		fmt.Println(output)
+	for i := 0; i < 5; i++ {
+		fmt.Println(text)
 	}
 }
